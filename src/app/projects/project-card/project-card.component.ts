128387f66b52692a395522f9efb5a9f4184f262b
MGajewski <gajewski.mike@gmail.com>
2018-02-05 16:30:45 -0500

    The project card component now takes all of its data in via Input properties.

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectCardComponent implements OnInit {
 
+  @Input() public avatar: string;
+  @Input() public title: string;
+  @Input() public subtitle: string;
+  @Input() public image: string;
+  @Input() public content: string;
+  @Input() public url: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * This function fires when the "view on github" button is clicked.
+   */
+  public gotoGithub() {
+    window.open(this.url, "_blank");
+  }
 }
